92e1cf880fc64addeaf45a819005fc9c07f1b84c
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-11-13 00:18:51 +0700

    init

--- resources/js/Components/TrixEditor.jsx
+++ resources/js/Components/TrixEditor.jsx
@@ -1,23 +1,37 @@
+// resources/js/Components/TrixEditor.jsx
 import { useEffect, useRef } from 'react'
 import 'trix/dist/trix.css'
 import 'trix'
 
 export default function TrixEditor({ value = '', onChange, inputName = 'note' }) {
   const inputRef = useRef(null)
+  const editorRef = useRef(null)
 
+  // sinkron ke react
   useEffect(() => {
-    const el = inputRef.current
+    const inputEl = inputRef.current
     const handler = (e) => {
       onChange && onChange(e.target.value)
     }
-    el.addEventListener('trix-change', handler)
-    return () => el.removeEventListener('trix-change', handler)
+    inputEl.addEventListener('trix-change', handler)
+    return () => inputEl.removeEventListener('trix-change', handler)
   }, [onChange])
+
+  // paksa arah teks LTR
+  useEffect(() => {
+    const editorEl = editorRef.current
+    if (editorEl) {
+      editorEl.setAttribute('dir', 'ltr')
+      editorEl.style.textAlign = 'left'
+    }
+  }, [])
 
   return (
     <>
+      {/* hidden input yang dipegang Trix */}
       <input id={inputName} type="hidden" defaultValue={value} ref={inputRef} />
-      <trix-editor input={inputName}></trix-editor>
+      {/* editor-nya */}
+      <trix-editor ref={editorRef} input={inputName}></trix-editor>
     </>
   )
 }
